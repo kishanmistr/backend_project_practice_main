4bd0655f2a447daea757f1b3bf773ebc61c14212
kiritDholakiya <dolakiyakirit@gmail.com>
2024-06-11 11:08:12 +0530

    four

---
+++ src/db/index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+
+    try {
+
+        const connectInstance = await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDB connected : ${connectInstance.connection.host}`);
+        
+    } 
+    
+    catch (error) {
+
+        console.log('mongodb connection failed',error);
+        process.exit(1);
+        
+    }
+
+}
+
+export { connectDB }
